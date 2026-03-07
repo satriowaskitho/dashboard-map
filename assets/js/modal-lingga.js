421d8f7b37ba9e84f8b6ff7ea43d81969da5492d
NatasyaAfira <75233133+NatasyaAfira@users.noreply.github.com>
2026-03-07 14:28:25 +0700

    Update modal-lingga.js

--- assets/js/modal-lingga.js
+++ assets/js/modal-lingga.js
@@ -112,7 +112,7 @@ function openModal(type, regionKey = "lingga", regionName = "Kepulauan Riau") {
 
   const charts = {
     ekonomi: () => {
-      title.textContent = `Pertumbuhan Ekonomi ${regionName} (%)`;
+      title.textContent = `Pertumbuhan Ekonomi ${regionName} (C to C) Triwulan IV (%)`;
       let dataArr = [];
       let labelsArr = dataEkonomi.tahun;
 
