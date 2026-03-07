c15bf6fc8bc0013be379b0f7b7276ce03a374135
NatasyaAfira <75233133+NatasyaAfira@users.noreply.github.com>
2026-03-07 14:29:21 +0700

    Update modal-batam.js

--- assets/js/modal-batam.js
+++ assets/js/modal-batam.js
@@ -118,7 +118,7 @@ function openModal(
 
   const charts = {
     ekonomi: () => {
-      title.textContent = `Pertumbuhan Ekonomi ${regionName} (%)`;
+      title.textContent = `Pertumbuhan Ekonomi ${regionName} (C to C) Triwulan IV (%)`;
       let dataArr = [];
       let labelsArr = dataEkonomi.tahun;
 
