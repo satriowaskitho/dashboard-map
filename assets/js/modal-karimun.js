36c7d6f0cbba1b60e2286f47956e59018eae0197
NatasyaAfira <75233133+NatasyaAfira@users.noreply.github.com>
2026-03-07 14:28:44 +0700

    Update modal-karimun.js

--- assets/js/modal-karimun.js
+++ assets/js/modal-karimun.js
@@ -120,7 +120,7 @@ function openModal(
 
   const charts = {
     ekonomi: () => {
-      title.textContent = `Pertumbuhan Ekonomi ${regionName} (%)`;
+      title.textContent = `Pertumbuhan Ekonomi ${regionName} (C to C) Triwulan IV (%)`;
       let dataArr = [];
       let labelsArr = dataEkonomi.tahun;
 
